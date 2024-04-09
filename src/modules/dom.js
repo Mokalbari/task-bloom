0cbc8458b2171bf4d7b96fa93a350545957773d7
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-04-09 16:47:42 +0200

    feat: form submission creates new Task
    - Add task to taskList array[]

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,8 +1,13 @@
+import { Task } from "./taskManager";
+
+let taskList = [];
+
 export class ModalDomReference {
   constructor() {
     this.openModalBtn = document.querySelector("#openModalBtn");
     this.closeModalBtn = document.querySelector("#closeModalBtn");
-    this.modalForm = document.querySelector("#modalForm");
+    this.modalDialogBox = document.querySelector("#modalDialogBox");
+    this.formContent = document.querySelector("#formContent");
   }
 }
 
@@ -11,17 +16,33 @@ export class ModalEvents extends ModalDomReference {
     super();
     this.initEvent();
     this.closeModal();
+    this.submitForm();
   }
 
   initEvent() {
     this.openModalBtn.addEventListener("click", () => {
-      this.modalForm.showModal();
+      this.modalDialogBox.showModal();
     });
   }
 
   closeModal() {
     this.closeModalBtn.addEventListener("click", () => {
-      this.modalForm.close();
+      this.modalDialogBox.close();
+    });
+  }
+
+  submitForm() {
+    this.formContent.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const title = document.getElementById("title").value;
+      const description = document.getElementById("description").value;
+      const dueDate = document.getElementById("due-date").value;
+      const priority = document.getElementById("priority").value;
+
+      const newTask = new Task(title, description, dueDate, priority);
+      this.modalDialogBox.close();
+      taskList.push(newTask);
+      console.table(taskList);
     });
   }
 }
